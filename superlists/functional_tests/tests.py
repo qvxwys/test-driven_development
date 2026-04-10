5265ed0a7a246df6581717871f82fdfeabbdf263
Dmitry Mikhnev <qvxwys@hotmail.com>
2026-04-10 18:15:46 +0300

    add new URL; view and template to display lists

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -29,8 +29,8 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        """Test. Можно начать список и получить его позже."""
+    def test_can_start_a_list_for_one_user(self):
+        """Test. Можно начать список для одного пользователя."""
 
         self.browser.get(self.live_server_url)
 
@@ -53,9 +53,40 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Сделать мушку из павльиных перьев')
         inputbox.send_keys(Keys.ENTER)
 
-        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
         self.wait_for_row_in_list_table(
             '2: Сделать мушку из павльиных перьев'
             )
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        """Тест: многочисленные пользователи могут начать списки по разным
+        url"""
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Сделать мушку', page_text)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Купить молоко')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Купить молоко', page_text)
 
         self.fail('Закончить тест!')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -26,21 +26,15 @@ class HomePageTest(TestCase):
         """Тест: переадресует после post-запроса."""
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(
+            response['location'],
+            '/lists/only_list/'
+            )
 
     def test_only_saves_items_when_necessary(self):
         """Тест: сохраняет элементы, только когда нужно."""
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
-        """Тест: отображаются все элементы списка."""
-        Item.objects.create(text='Itemey 1')
-        Item.objects.create(text='Itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('Itemey 1', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -63,3 +57,22 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+    """Тест представления списка."""
+
+    def test_uses_list_template(self):
+        """Тест: используется шаблон списка."""
+        response = self.client.get('/lists/only_list/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        """Тест: отображаются все элементы списка."""
+        Item.objects.create(text='Itemey 1')
+        Item.objects.create(text='Itemey 2')
+
+        response = self.client.get('/lists/only_list/')
+
+        self.assertContains(response, 'Itemey 1')
+        self.assertContains(response, 'Itemey 2')
