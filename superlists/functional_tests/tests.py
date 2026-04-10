e234df2ced0c51c650b9d721a126d54a66549a55
Dmitry Mikhnev <qvxwys@hotmail.com>
2026-04-10 16:46:29 +0300

    add constants

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -2,8 +2,11 @@ import time
 
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+from .constants import MAX_WAIT
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,11 +15,19 @@ class NewVisitorTest(LiveServerTestCase):
         """Initialize Chrome WebDriver."""
         self.browser = webdriver.Chrome()
 
-    def check_for_row_in_list_table(self, row_text):
-        """Подтверджение строки в таблице списка."""
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        """Ожидание строки в таблице списка."""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Test. Можно начать список и получить его позже."""
@@ -36,24 +47,14 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Купить павлиньи перья')
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
 
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Сделать мушку из павльиных перьев')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        # table = self.browser.find_element(By.ID, 'id_list_table')
-        # rows = table.find_elements(By.TAG_NAME, 'tr')
-        # self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        # self.assertIn(
-        #     '2: Сделать мушку из павльиных перьев',
-        #     [row.text for row in rows]
-        #     )
-
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table(
             '2: Сделать мушку из павльиных перьев'
             )
 
